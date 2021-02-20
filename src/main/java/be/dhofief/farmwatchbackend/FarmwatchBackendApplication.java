89d64836dfa1cd7962b4907eb5201a798a247833
kjdhoore <kjell.dhoore@ugent.be>
2021-02-20 20:47:41 +0100

    second initial commit

---
+++ src/main/java/be/dhofief/farmwatchbackend/FarmwatchBackendApplication.java
@@ -0,0 +1,13 @@
+package be.dhofief.farmwatchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmwatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmwatchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/dhofief/farmwatchbackend/FarmwatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dhofief.farmwatchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmwatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
